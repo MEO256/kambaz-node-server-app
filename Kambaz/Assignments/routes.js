724a4fcad083984f3c0cc10f1e0c31849a0cbabc
Maxim Onashchenko <132910564+MEO256@users.noreply.github.com>
2025-04-02 21:19:42 -0400

    Add files via upload

---
+++ Kambaz/Assignments/routes.js
@@ -0,0 +1,30 @@
+import * as dao from "./dao.js";
+
+export default function AssignmentRoutes(app) {
+
+    app.put("/api/assignments/:aid", async (req, res) => {
+        const { aid } = req.params;
+        const assignment = req.body;
+        const status = await dao.updateAssign(aid, assignment);
+        res.send(status);
+    });
+    
+
+    app.delete("/api/assignments/:aid", async (req, res) => {
+        const { aid } = req.params;
+        const status = await dao.deleteAssignment(aid);
+        res.send(status);
+    });
+    
+    app.post("/api/courses/:cid/assignments", async (req, res) => {
+        const { cid } = req.params;
+        const module = await dao.createAssignment(cid, req.body);
+        res.json(module);
+    });
+    
+  app.get("/api/courses/:cid/Assignments", async (req, res) => {
+    const { cid } = req.params;
+    const status = await dao.findAssignByCid(cid);
+    res.send(status);
+  });
+}
